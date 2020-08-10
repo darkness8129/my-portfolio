4a9b9eb16ad9a32a3925114e947b6607c1fd068e
darkness8129 <yaroslaw460@gmail.com>
2020-08-11 01:22:59 +0300

    animation on toggle btn added

--- js/header.js
+++ js/header.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     $('.navbar-toggler').on('click', function(){
-        $( '.header' ).toggleClass( 'header-active' );      
+        $( '.header' ).toggleClass( 'header_active' );   
+        $('.toggler-icon').toggleClass('toggler-icon_active');   
     });
 });
