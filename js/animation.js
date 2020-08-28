e80603284d52cb12d044a6355ccd01f2fd7da458
darkness8129 <yaroslaw460@gmail.com>
2020-08-28 18:38:20 +0300

    rebuild of portfolio and some other changes

--- js/animation.js
+++ js/animation.js
@@ -1,7 +1,7 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $("body").css("display", "none");
-    $("body").fadeIn(2000);           
-    $("a").click(function(event){
+    $("body").fadeIn(2000);
+    $("a").click(function (event) {
         event.preventDefault();
         linkLocation = this.href;
         $("body").fadeOut(1000, redirectPage);
--- js/header.js
+++ js/header.js
@@ -1,26 +1,26 @@
-$(document).ready(function(){
-    $('.navbar-toggler').on('click', function(){
-        
+$(document).ready(function () {
+    $('.navbar-toggler').on('click', function () {
+
         //header fullscreen
-        $( '.header' ).toggleClass( 'header_active' ); 
+        $('.header').toggleClass('header_active');
 
         //for burger animation 
-        $('.toggler-icon').toggleClass('toggler-icon_active');   
+        $('.toggler-icon').toggleClass('toggler-icon_active');
 
         //for header title when header active
-        if(window.innerWidth <= 575 && $('.header').hasClass('header_active')){
+        if (window.innerWidth <= 575 && $('.header').hasClass('header_active')) {
             $('.header-title-mobile').css('display', 'none');
-        }else if(window.innerWidth <= 575){
+        } else if (window.innerWidth <= 575) {
             $('.header-title-mobile').css('display', 'block');
         }
-        
+
     });
-    
+
     //for header title on diff width
-    $(window).on('resize', function(){
-        if(window.innerWidth > 575){
+    $(window).on('resize', function () {
+        if (window.innerWidth > 575) {
             $('.header-title-mobile').css('display', 'none');
-        }else if(!$('.header').hasClass('header_active')){
+        } else if (!$('.header').hasClass('header_active')) {
             $('.header-title-mobile').css('display', 'block');
         }
     });
