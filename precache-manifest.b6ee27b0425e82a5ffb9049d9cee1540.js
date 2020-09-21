c7f6d76a63da926f5bf41b7b11f99bc9ba341843
darkness8129 <yaroslaw460@gmail.com>
2020-09-21 22:37:59 +0300

    Updates

---
+++ precache-manifest.b6ee27b0425e82a5ffb9049d9cee1540.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36d25770bcd59ab59728a04ca7cfd4a2",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "a35c028489529385a350",
+    "url": "/my-portfolio/static/css/main.590701a6.chunk.css"
+  },
+  {
+    "revision": "4b96e25c254439c9317a",
+    "url": "/my-portfolio/static/js/2.2588c0a1.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/my-portfolio/static/js/2.2588c0a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a35c028489529385a350",
+    "url": "/my-portfolio/static/js/main.4d813d2f.chunk.js"
+  },
+  {
+    "revision": "f241adf7bba56ceeeb82",
+    "url": "/my-portfolio/static/js/runtime-main.8b00eace.js"
+  }
+]);
